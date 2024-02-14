6df094a5d2ce36bb5397069922b0fd53f834456e
Spacewalker215 <141846819+Spacewalker215@users.noreply.github.com>
2024-02-14 06:25:43 -0500

    Bug Fix
    Fixed bug where you can't see the first question

--- script.js
+++ script.js
@@ -67,7 +67,6 @@ if('speechSynthesis' in window){
                 questions.push(questionCell.v.toString().trim());
                 answers.push(answerCell.v.toString().trim());
             }
-    
             return { questions, answers };
         }
 
